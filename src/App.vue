e2ed6020b6dd8edb6e3d3822c8ac222ef365dbba
Chambers <bobhus394@gmail.com>
2025-06-12 13:31:07 +0800

    完成基本問題
    - 使用radio來製作選擇按鈕
    - 使用gird做棋盤的排版設計
    - 走線動畫元素切出來用元件呈現
    - 切換方格按鈕後可以正常呈現指定數量的走線元件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,85 @@
 <template>
 <main>
-  <h1>Hi</h1>
+  <div class="container">
+    <BoxComponent v-for="(item, index) in allSquares" :key="index + '-' + allSquares"/>
+  </div>
+  <div class="btn-group">
+    <label class="btn" v-for="size in allSizes" :class="{ active: selectSize === size }" :key="size">
+      <input class="sr-only" type="radio" :value="size" name="size" v-model.number="selectSize">
+      <span>{{ size }}x{{ size }}</span>
+    </label>
+  </div>
+  <div class="btn-group">
+    <label class="btn" v-for="type in types" :key="type" :class="{ active: selectType === type }" >
+      <input class="sr-only" type="radio" :value="type" name="type" v-model="selectType">
+      <span>{{ type }}</span>
+    </label>
+  </div>
 </main>
 </template>
 
 
 <script setup>
+import { computed, ref } from 'vue'
+import BoxComponent from './components/BoxComponent.vue'
+const selectSize = ref(1)
+const allSizes = [1, 3, 5, 10]
+
+const allSquares = computed(() => selectSize.value * selectSize.value)
+
+const types = ['All', 'Random']
+const selectType = ref('All')
 </script>
 
 <style scoped>
+.container {
+  padding: 20px 0 0;
+  width: 90%;
+  margin: 0 auto 20px;
+  aspect-ratio: 1 / 1;
+  display: grid;
+  grid-template-columns: repeat(v-bind(selectSize), 1fr);
+  grid-template-rows: repeat(v-bind(selectSize), 1fr);
+  gap: 4px;
+}
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+}
+
+.btn-group {
+  width: 90%;
+  margin: auto;
+  margin-bottom: 20px;
+}
+.btn {
+  display: inline-block;
+  background-color: var(--text-color);
+  color: var(--background-color);
+  cursor: pointer;
+  font-size: 16px;
+  line-height: 2;
+  padding: 0 16px;
+  border-radius: 6px;
+  transition: opacity .3s ease, background .3s ease;
+  &:hover {
+    opacity: .9;
+  }
+  &:active {
+    opacity: .8;
+  }
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+}
+.btn.active {
+  background-color: #b39cd0;
+}
 </style>
---
+++ src/components/BoxComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="box">
+    <div class="content"></div>
+  </div>
+</template>
+
+
+<script setup>
+</script>
+
+<style scoped>
+
+@property --angle {
+  syntax: '<angle>';
+  inherits: false;
+  initial-value: 0deg;
+}
+.box {
+  width: 100%;
+  height: 100%;
+  background-color: #aaa;
+  box-sizing: border-box;
+  padding: 2px;
+  border-radius: 10px;
+  background-image: conic-gradient(
+    from var(--angle),
+    white 0deg,
+    white 30deg,
+    transparent 30deg,
+    transparent 180deg,
+    white 180deg,
+    white 210deg,
+    transparent 210deg,
+    transparent 360deg
+  );
+  animation: spin-angle 1s ease infinite;
+}
+@keyframes spin-angle {
+  0% {
+    --angle: 0deg;
+  }
+  100% {
+    --angle: 180deg;
+  }
+}
+.content {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  background-color: var(--background-color);
+}
+</style>
